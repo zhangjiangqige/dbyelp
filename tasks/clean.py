8429bc7e30cad7b49dd5e5a860dae8ff3f357358
zhangjiangqige <zhangjiangqige@gmail.com>
2019-04-24 16:46:43 -0400

    clean users with wrong avg star

--- tasks/clean.py
+++ tasks/clean.py
@@ -17,9 +17,54 @@ fixes = {
                 from tip
                 group by business_id, user_id, date
             );''',
-            'alter table `tip_fix_tip-dup-entry` add primary key (business_id, user_id, date);',
             'drop table tip;',
             'rename table `tip_fix_tip-dup-entry` to tip;'
+        ]
+    },
+    'remove-user-with-wrong-avg-star': {
+        'description': 'Some users have an average star with large difference from that calculated from the table review. This fix removes these kind of user from the tables user and review.',
+        'statement_check': '''
+            select user.user_id, user.average_stars, rev.a_star
+            from user inner join
+            (select user_id, avg(stars) as a_star
+            from review
+            group by user_id) as rev on user.user_id = rev.user_id
+            where abs(user.average_stars - rev.a_star) > 1;
+        ''',
+        'statements': [
+            '''
+            create table user_tmp (
+                average_stars float default 0,
+                cool int default 0,
+                elite VARCHAR(1024) default null,
+                fans int default 0,
+                review_count int default 0,
+                useful int default 0,
+                user_id char(32) not null,
+                yelping_since datetime
+            );''',
+            '''
+            insert into user_tmp (
+                select * from user where user_id not in (select user_id from user_with_wrong_average_stars)
+            );''',
+            'drop table user;',
+            'rename table user_tmp to user;',
+            '''
+            create table review_tmp (
+                business_id char(32) not null,
+                cool int default 0,
+                date datetime not null,
+                funny int default 0,
+                review_id char(32) not null,
+                stars int default 0,
+                useful int default 0,
+                user_id char(32) not null
+            );''',
+            '''insert into review_tmp (
+                select * from review where user_id not in (select user_id from user_with_wrong_average_stars)
+            );''',
+            'drop table review;',
+            'rename table review_tmp to review;'
         ]
     }
 }
