54e1b9fadc3ff2be38c45a9748ff7634157f8ecd
zhangjiangqige <zhangjiangqige@gmail.com>
2019-04-24 17:03:58 -0400

    list clean tasks

--- views/views.py
+++ views/views.py
@@ -1,7 +1,22 @@
 from app import app
 from flask import jsonify, request
 
+from tasks import clean
+
 
 @app.route('/')
 def index():
     return jsonify({'success': 1})
+
+
+@app.route('/clean/tasks')
+def clean_tasks():
+    return jsonify({
+        'success': 1,
+        'tasks': [
+            {
+                'name': k,
+                'description': v['description']
+            } for k, v in clean.fixes.items()
+        ]
+    })
