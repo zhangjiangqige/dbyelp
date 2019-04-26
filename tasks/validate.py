70c3a3f67a093592eb162a4d81402b32efed7166
zhangjiangqige <zhangjiangqige@gmail.com>
2019-04-26 15:40:32 -0400

    still use table to store train/val

--- tasks/validate.py
+++ tasks/validate.py
@@ -15,10 +15,12 @@ def split_train_val():
 
 def split_business():
     queries = [
-        'drop view if exists business_train_view',
-        'drop view if exists business_val_view',
-        'create view business_train_view as select * from business limit 100000',
-        'create view business_val_view as select * from business limit 200000 offset 100000',
+        'drop table if exists business_train',
+        'drop table if exists business_val',
+        'create table business_train like business',
+        'create table business_val like business',
+        'insert into business_train (select * from business limit 100000)',
+        'insert into business_val (select * from business limit 200000 offset 100000)'
     ]
     for q in queries:
         dbutils.execute(q, print=True)
@@ -26,10 +28,12 @@ def split_business():
 
 def split_review():
     queries = [
-        'drop view if exists review_train_view',
-        'drop view if exists review_val_view',
-        'create view review_train_view as select * from review where business_id in (select business_id from business_train_view)',
-        'create view review_val_view as select * from review where business_id in (select business_id from business_val_view)'
+        'drop table if exists review_train;',
+        'drop table if exists review_val;',
+        'create table review_train like review;',
+        'create table review_val like review;',
+        'insert into review_train (select * from review where business_id in (select business_id from business_train));',
+        'insert into review_val (select * from review where business_id in (select business_id from business_val));'
     ]
     for q in queries:
         dbutils.execute(q, print=True)
